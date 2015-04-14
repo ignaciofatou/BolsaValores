07b1c90b4e24971614a364c9cb263b1df025f82f
Ignacio <Ignacio@Ignacio-PC>
2015-04-14 19:19:10 +0200

    Algunos cambios

---
+++ src/bolsa/Fecha.java
@@ -0,0 +1,43 @@
+package bolsa;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Fecha {
+    
+    private static final String YYYYMMDD = "YMd";
+    private static final String HH24MISS = "Hmmss";
+    
+    //Retorna la Fecha y Hora Actual YYYYMMDD_HH24MISS -> 2015320_215625
+    public static String getFechaHora(){
+        //Obtener la fecha y hora actual
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat formatoFechaPersonal;
+        formatoFechaPersonal = new SimpleDateFormat(YYYYMMDD + "_" + HH24MISS);
+        return formatoFechaPersonal.format(cal.getTime());
+    }
+    //Retorna la Fecha Actual YYYYMMDD -> 2015320
+    public static String getFecha(){
+        //Obtener la fecha y hora actual
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat formatoFechaPersonal;
+        formatoFechaPersonal = new SimpleDateFormat(YYYYMMDD);
+        return formatoFechaPersonal.format(cal.getTime());
+    }
+    //Retorna la Hora Actual HH24MISS -> 215625
+    public static String getHora(){
+        //Obtener la fecha y hora actual
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat formatoFechaPersonal;
+        formatoFechaPersonal = new SimpleDateFormat(HH24MISS);
+        return formatoFechaPersonal.format(cal.getTime());
+    }
+    //Retorna la Fecha/Hora en el Formato Especificado
+    public static String getFechaHora(String formato){
+        //Obtener la fecha y hora actual
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat formatoFechaPersonal;
+        formatoFechaPersonal = new SimpleDateFormat(formato);
+        return formatoFechaPersonal.format(cal.getTime());
+    }
+}
